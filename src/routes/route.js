db97f16db4d16e4f18f0dbeaf49aecd45c0897d3
Tokeshwar <tokeshwar007@gmail.com>
2022-02-22 21:45:17 +0530

    22FebAssignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,7 +7,26 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
+// 1. API will return list of movies in Array
+router.get('/movies', function(req, res){
+    res.send('["Lucifer", "Pirates of the carribien", "Little things", "Mirzapur", "Money Heist"]')
+});
 
+// 2. API will return movei by index in Array
+router.get('/movies/:movieID', function(req, res){
+    mov = ["Lucifer", "Pirates of the carribien", "Little things", "Mirzapur", "Money Heist"]
+    let value = req.params.movieID
+    if(value>mov.length-1){
+        res.send("movie not found")
+    } else {
+        res.send(mov[value])
+    }
+});
+
+// 4. Return an Array
+router.get('/movie', function(req, res){
+    res.send([ {id:1,name: 'The Shining'}, {id:2,name: 'Incendies'}, {id:3,name: 'Rang de Basanti'}, {id:4,name: 'Finding Demo'} ])
+})
 
 
 module.exports = router;
