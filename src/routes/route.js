3f22d5fb38a316200c4f0ad3397c06f23586e74a
Tokeshwar <tokeshwar007@gmail.com>
2022-02-26 17:38:51 +0530

    24Feb_Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+// ASSIGNMENT:
+// you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+// also return an array consisting of only the person that can vote
 
+let persons= [
+   {
+   name: "PK",
+   age: 10,
+   votingStatus: false
+},
+{
+   name: "SK",
+   age: 20,
+   votingStatus: false
+},
+{
+   name: "AA",
+   age: 70,
+   votingStatus: false
+},
+{
+   name: "SC",
+   age: 5,
+   votingStatus: false
+},
+{
+   name: "HO",
+   age: 40,
+   votingStatus: false
+}
+]
+ 
+router.post('/voting', function(req, res){
+    let votingAge = req.query.votingAge
 
+    let a = [];
+    for (let i=0; i<persons.length; i++){
+        if (persons[i].age > votingAge){
+            persons[i].votingStatus = true
+            a.push(persons[i])
+        }
+    }
+    if(a.length>0)
+    {
+        return res.send(a)
+    } else{
+        return res.send("no member found")
+    }
+});
 
 
 module.exports = router;
+
