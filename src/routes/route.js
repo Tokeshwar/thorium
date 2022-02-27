cdef0b17ffb728d9cea8ad1ea57532f59e2bc37c
Tokeshwar <tokeshwar007@gmail.com>
2022-02-28 01:02:26 +0530

    25feb_assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const UserModel= require("../models/userModel.js")
+const UserController= require("../controllers/userController")
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
 })
 
+router.post("/createUser", UserController.createUser  )
 
-
+router.get("/getUsersData", UserController.getUsersData)
 
 module.exports = router;
