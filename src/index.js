d46343d28ec2f0fc5520b88f2dfa4ab9ad1dc479
Tokeshwar <tokeshwar007@gmail.com>
2022-03-17 08:40:53 +0530

    blog project

--- src/index.js
+++ src/index.js
@@ -8,22 +8,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ranjan:e91pDMx03Sx9wB2V@cluster0.u4idw.mongodb.net/Tokeshwar-db?authSource=admin&replicaSet=atlas-oajyb4-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-// app.use (
-//     function (req, res, next) {
-//         console.log ("inside GLOBAL MW");
-//         next();
-//   }
-//   );
-
-app.use('/', route);
+ app.use('/', route);
 
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+})
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,28 @@
-const express = require('express');
-const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const express = require('express'); 
+const router = express.Router(); 
+
+const authorController = require('../controller/authorController'); 
+const blogController = require('../controller/blogController'); 
+const authorLogin = require('../controller/loginController');
+const middleware = require('../middlewares/middleware');
 
 
+router.post("/createAuthor", authorController.createAuthor)
+router.post("/login", authorLogin.login)
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.post("/crtBlog", middleware.authenticate, blogController.createBlog)
 
-
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
-
-router.post("/cowin/getOtp", CowinController.getOtp)
-
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-
-
+router.get("/getBlogs", middleware.authenticate, middleware.authorize, blogController.getBlogs)
+router.put("/updateBlog/:blogId", middleware.authenticate, middleware.authorize, blogController.updateBlog)
+router.delete("/deleteBlogById/:blogId",middleware.authenticate, middleware.authorize, blogController.deleteBlogById)
+router.delete("/deleteByQueryParams",middleware.authenticate, middleware.authorize, blogController.deletedByQueryParams)
+router.get("/getAllBlogs", blogController.getAllBlogs)
 
 module.exports = router;
+
+
+
+
+
+
+
